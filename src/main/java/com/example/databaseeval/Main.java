c61e2fb6152802c8438dcb99ddf93d3effaf4c59
KarunyaGupta <mails.karunyagupta@gmail.com>
2025-08-27 19:24:45 +0530

    changes done in main

--- src/main/java/com/example/databaseeval/Main.java
+++ src/main/java/com/example/databaseeval/Main.java
@@ -3,6 +3,7 @@ package com.example.databaseeval;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
